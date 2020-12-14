6edf579ad9907575a75ce1fdad4fb011b26585dc
Maximilian Kaske <maximilian@kaske.org>
2020-12-14 23:38:23 +0100

    :wrench: add styles

---
+++ src/styles/index.ts
@@ -0,0 +1,37 @@
+export const Spacings = {
+  xs: 2,
+  s: 4,
+  m: 8,
+  l: 16,
+  xl: 24,
+};
+
+export const Colors = {
+  primary: "#2292A4",
+  secondary: "#BDBF09",
+  tertiary: "#D96C06",
+  mainForeground: "#0F0A0A",
+  mainBackground: "white",
+  cardBackground: "#F5EFED",
+};
+
+export const Typography = {
+  body: {
+    // fontFamily
+    fontSize: 18,
+    lineHeight: 20,
+    color: Colors.mainForeground,
+  },
+  title: {
+    // fontFamily
+    fontSize: 24,
+    lineHeight: 28,
+    color: Colors.primary,
+  },
+  subtitle: {
+    // fontFamily
+    fontSize: 20,
+    lineHeight: 24,
+    color: Colors.secondary,
+  },
+};
