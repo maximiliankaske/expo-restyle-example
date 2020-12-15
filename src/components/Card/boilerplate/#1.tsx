c7c91b2fea1994a8b9dc996b18a4314b8e6a5c3c
Maximilian Kaske <maximilian@kaske.org>
2020-12-15 22:23:01 +0100

    :construction: add boilerplate for versions

---
+++ src/components/Card/boilerplate/#1.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { View, Text, Image, ImageSourcePropType } from "react-native";
+import { Typography, Colors, Spacings } from "../../../styles";
+
+interface CardProps {
+  title: string;
+  subtitle: string;
+  date: string;
+  imgSrc: ImageSourcePropType;
+}
+
+const Card = ({ title, subtitle, imgSrc, date }: CardProps) => {
+  return (
+    <View
+      style={{
+        flexDirection: "row",
+        padding: Spacings.m,
+        margin: Spacings.m,
+        backgroundColor: Colors.cardBackground,
+        borderRadius: Spacings.l,
+      }}
+    >
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          paddingRight: Spacings.m,
+        }}
+      >
+        <Text style={{ ...Typography.title, textAlign: "center" }}>
+          {title}
+        </Text>
+        <Text
+          style={{
+            ...Typography.subtitle,
+            textAlign: "center",
+            paddingBottom: Spacings.l,
+          }}
+        >
+          {subtitle}
+        </Text>
+        <Text style={{ ...Typography.body, textAlign: "center" }}>{date}</Text>
+      </View>
+      <Image
+        source={imgSrc}
+        style={{ width: 150, height: 150, borderRadius: 75 }}
+      />
+    </View>
+  );
+};
+
+export default Card;
---
+++ src/components/Card/boilerplate/#2.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import {
+  View,
+  Text,
+  Image,
+  ImageSourcePropType,
+  StyleSheet,
+} from "react-native";
+import { Typography, Colors, Spacings } from "../../../styles";
+
+interface CardProps {
+  title: string;
+  subtitle: string;
+  date: string;
+  imgSrc: ImageSourcePropType;
+}
+
+const Card = ({ title, subtitle, imgSrc, date }: CardProps) => {
+  return (
+    <View style={styles.card}>
+      <View style={styles.textContainer}>
+        <Text style={styles.title}>{title}</Text>
+        <Text style={styles.subtitle}>{subtitle}</Text>
+        <Text style={styles.date}>{date}</Text>
+      </View>
+      <Image source={imgSrc} style={styles.image} />
+    </View>
+  );
+};
+
+export default Card;
+
+const styles = StyleSheet.create({
+  card: {
+    flexDirection: "row",
+    padding: Spacings.m,
+    margin: Spacings.m,
+    backgroundColor: Colors.cardBackground,
+    borderRadius: Spacings.l,
+  },
+  textContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    paddingRight: Spacings.m,
+  },
+  title: {
+    ...Typography.title,
+    textAlign: "center",
+  },
+  subtitle: {
+    ...Typography.subtitle,
+    textAlign: "center",
+    paddingBottom: Spacings.l,
+  },
+  date: { ...Typography.body, textAlign: "center" },
+  image: { width: 150, height: 150, borderRadius: 75 },
+});
